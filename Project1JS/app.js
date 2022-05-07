554fc722670afa5e3ad28399e10f9eef73904058
shueijo <jshue23@gmail.com>
2022-05-07 18:58:57 +1000

    API working, display temp and other data

--- Project1JS/app.js
+++ Project1JS/app.js
@@ -2,23 +2,26 @@
 window.addEventListener('load', ()=> {
     let long;
     let lat;
+    let tempDegree = document.querySelector(".temp-degree"); 
+    let locationTimezone = document.querySelector(".location-timezone");
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-
-            const proxy = 'https://cors-anywhere.herokuapp.com';
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=723554c5ccd52d5e9d82d54d042f7a9c`;
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=723554c5ccd52d5e9d82d54d042f7a9c&units=metric`;
 
 //Grab data from API and return
             fetch(api) 
-                .then(response =>{
+                .then((response) =>{
                     return response.json();
                 })
                 .then(data =>{
-                    console.log(data)
-                })
+                console.log(data);
+                const {temp} = data.main;
+                //Set DOM Elements from the API
+                tempDegree.textContent = temp;
+                });
             });
         }
 });

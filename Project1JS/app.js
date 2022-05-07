c60fa27e911e8274c73c8e370d9e24424412e5f7
shueijo <jshue23@gmail.com>
2022-05-07 17:07:49 +1000

    API integration

--- Project1JS/app.js
+++ Project1JS/app.js
@@ -1,10 +1,22 @@
+//Grab user's location on webpage load
 window.addEventListener('load', ()=> {
     let long;
     let lat;
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
-        });
-    }
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const api = `https://api.weatherbit.io/v2.0/current?${lat}&${long}&key=6a946beeb18042fa9ea6362592f04763`
+
+            fetch(api) 
+                .then(response =>{
+                    return response.json();
+                })
+                .then(data =>{
+                    console.log(data)
+                })
+            });
+        }
 });

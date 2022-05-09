76ea790871cb5ae370eb1569f30b4798d8cb042f
shueijo <jshue23@gmail.com>
2022-05-09 20:53:51 +1000

    Trying skycons again

--- Project1JS/app.js
+++ Project1JS/app.js
@@ -28,9 +28,48 @@ window.addEventListener('load', ()=> {
                 tempDegree.textContent = temp;
                 locationTimezone.textContent = country;
                 tempDescription.textContent = description;
-                //Set Icon 
-                setIcons(description, document.querySelector(".icon"));
                 });
             });
         }
+
+        
+    function iconDisplay() {
+    // Calling skycons variables
+        let skycons1 = new Skycons({"color": "#3da4ab"});
+        let skycons2 = new Skycons({"color": "#3EB890"});
+        let skycons3 = new Skycons({"color": "#E8A723"});
+        let skycons4 = new Skycons({"color": "#472A49"});
+        let skycons5 = new Skycons({"color": "#65863A"});
+        let skycons6 = new Skycons({"color": "#CC2027"});
+    //Display and play icon according to API's weather conditions
+        if (id < 250){ 
+            skycons1.add("icon1", skycons.RAIN)
+            skycons1.play();
+        } 
+        else if (id < 350){
+            skycons1.add("icon1", skycons.RAIN)
+            skycons1.play();
+        }
+        else if (id < 550){
+            skycons1.add("icon1", skycons.RAIN)
+            skycons1.play();
+        }
+        else if (id < 650){
+            skycons2.add("icon2", skycons.SNOW)
+            skycons2.play();
+        }
+        else if (id < 800){
+            skycons3.add("icon3", skycons.FOG)
+            skycons3.play();
+        }
+        else if (id === 800){
+            skycons4.add("icon4", skycons.CLEAR_DAY)
+            skycons4.play();
+        }
+        else if (id > 800){
+            skycons5.add("icon5", skycons.CLOUDY)
+            skycons5.play();
+        }
+    }      
+    
 });
